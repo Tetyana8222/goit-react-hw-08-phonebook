0d57cf67b21af3b96431fabc553c24b9ca42c781
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-06-20 15:57:01 +0300

    add

--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import phonebook from '../img/phonebook.png';
 
 const HomeView = () => {
   console.log('hello');
   return (
     <div>
       <h1>Welcome to your phonebook</h1>
+      <img src={phonebook} alt="phonebook" />
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   FLUSH,
@@ -12,12 +12,23 @@ import {
 import logger from 'redux-logger';
 import { contactsReducer } from './contactsSlice';
 import { filterSlice } from './filterSlice';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterSlice.reducer,
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware({
       serializableCheck: {
