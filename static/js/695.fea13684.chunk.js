dac11dd71f0a33e0be9e23d5c0d1201a8f11e781
Tetyana8222 <113339141+Tetyana8222@users.noreply.github.com>
2023-06-30 07:45:07 +0000

    Deploying to gh-pages from @ Tetyana8222/goit-react-hw-08-phonebook@fe9e51c61072b6181c8170443aaaf06a51a295cf 🚀

---
+++ static/js/695.fea13684.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[695],{5934:function(e,o,t){t.r(o),t.d(o,{default:function(){return c}});t(2791);var n=t.p+"static/media/phonebook.5cff79def9474b96aa03.png",r=t(184),c=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Welcome to your phonebook"}),(0,r.jsx)("img",{src:n,alt:"phonebook"})]})}}}]);
+//# sourceMappingURL=695.fea13684.chunk.js.map
